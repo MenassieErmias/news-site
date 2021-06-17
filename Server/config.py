7bd7e7192689a564a9a2fceb4be733de03160c19
nigustsegay <82320345+nigustsegay@users.noreply.github.com>
2021-06-17 10:51:51 +0300

    Update config.py

--- Server/config.py
+++ Server/config.py
@@ -5,6 +5,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 ENV = 'dev'
 if ENV == 'dev':
